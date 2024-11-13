4853cb53ed4b9f595834e444eb6325d1f7b3dfa3
jhw0900 <jhw0900@gmail.com>
2024-11-13 20:43:49 +0900

    fixed layout error

--- src/layouts/default-layout.jsx
+++ src/layouts/default-layout.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const Layout = ({ children }) => {
@@ -17,7 +17,7 @@ const Layout = ({ children }) => {
   )
 }
 
-const styles = const styles = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
       flex: 1, 
       backgroundColor: "#fff",
--- src/layouts/hoc/defaultLayout.jsx
+++ src/layouts/hoc/defaultLayout.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from "../default-layout";
 
-  return (
 const defaultLayout = (Component) => {
   return (props) => (
     <Layout>
