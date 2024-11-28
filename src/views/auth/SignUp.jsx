aab98d0544d6652332b93b342c0acfe9a0011a26
JHW0900 <JHW0900@gmail.com>
2024-11-28 13:20:29 +0900

    회원가입 구현

--- src/views/auth/SignUp.jsx
+++ src/views/auth/SignUp.jsx
@@ -1,9 +1,17 @@
-import { View, StyleSheet, TouchableWithoutFeedback, Keyboard } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import React, { useLayoutEffect, useState } from "react";
 import { useToast } from "react-native-toast-notifications";
 
+// IMPORT CONFIGS
+import { API } from "config/fetch.config";
+
 // IMPORT RESOURCES
-import { theme } from "../../resources/theme/common";
+import { theme } from "resources/theme/common";
 
 // IMPORT COMPONENTS
 import Button from "components/common/Button";
@@ -12,7 +20,7 @@ import Input from "components/common/Input";
 
 const SignUp = ({ navigation }) => {
   const toast = useToast();
-  const [nickname, setNickname] = useState("");
+  const [user_nickname, setUserNickname] = useState("");
   const [user_name, setName] = useState("");
   const [user_email, setEmail] = useState("");
   const [user_pw, setPassword] = useState("");
@@ -29,13 +37,30 @@ const SignUp = ({ navigation }) => {
     });
   });
 
-  const handleSignUp = () => {
+  const handleSignUp = async () => {
     if (user_pw !== confirmPassword) {
       toast.show("비밀번호가 일치하지 않습니다.");
       return;
     }
-    toast.show("회원가입이 완료되었습니다.");
-    navGo.to("SignIn");
+
+    res = await API.PUT({
+      url: "/userInfo",
+      data: {
+        user_id: user_email,
+        user_email,
+        user_pw,
+        user_name,
+        user_nickname,
+        user_birth: Date.now(),
+      },
+    });
+
+    if (res) {
+      toast.show("회원가입이 완료되었습니다.");
+      navGo.to("SignIn");
+    } else {
+      toast.show("다시 시도해주세요.");
+    }
   };
 
   return (
@@ -47,8 +72,8 @@ const SignUp = ({ navigation }) => {
           <Label style={{ width: "35%", marginBottom: 15 }} text="닉네임 " />
           <Input
             style={{ width: "65%" }}
-            value={nickname}
-            onChangeText={(text) => setNickname(text)}
+            value={user_nickname}
+            onChangeText={(text) => setUserNickname(text)}
           />
         </View>
         <View style={styles.inputContainer}>
@@ -78,7 +103,10 @@ const SignUp = ({ navigation }) => {
           />
         </View>
         <View style={styles.inputContainer}>
-          <Label style={{ width: "35%", marginBottom: 15 }} text="비밀번호 확인 " />
+          <Label
+            style={{ width: "35%", marginBottom: 15 }}
+            text="비밀번호 확인 "
+          />
           <Input
             style={{ width: "65%" }}
             value={confirmPassword}
