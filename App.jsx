3e3f0000f64289025aa107f8dd3f097aee2dac42
jhw0900 <jhw0900@gmail.com>
2024-11-29 19:41:19 +0900

    Toast UI/UX 수정

--- App.jsx
+++ App.jsx
@@ -2,13 +2,28 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/layouts/navigator/AppNavigator";
 import { ToastProvider } from "react-native-toast-notifications";
+import { StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <ToastProvider offsetBottom={100} duration={3000}>
+    <ToastProvider
+      offsetBottom={100}
+      duration={1500}
+      style={styles.toastContainer}
+    >
       <NavigationContainer>
         <AppNavigator />
       </NavigationContainer>
     </ToastProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  toastContainer: {
+    width: "85%", // 고정된 너비 설정
+    backgroundColor: "rgba(0, 0, 0, 0.6)", // 배경색 설정
+    borderRadius: 20, // 테두리 둥글게 설정
+    alignItems: "center", // 텍스트 가운데 정렬
+    justifyContent: "center", // 세로 방향 중앙 정렬
+  },
+});
