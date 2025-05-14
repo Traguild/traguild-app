e0434e02cd5785d9bbc1b4cd1952e6fae142736b
Howon Jeong <one.0x393@gmail.com>
2025-05-14 14:19:40 +0900

    feat(Report): implement report process

--- src/views/common/Report.jsx
+++ src/views/common/Report.jsx
@@ -1,11 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React, { useLayoutEffect } from "react";
+
+// IMPORT CONFIGS
+import { API } from "config/fetch.config";
 
 // IMPORT RESOURCES
 import { theme } from "resources/theme/common";
+import { useToast } from "react-native-toast-notifications";
 
 const Report = ({ navigation, route }) => {
+  const toast = useToast();
   const req = route.params;
+  const filter = ["비매너", "성희롱", "욕설", "음란물", "사기", "기타"];
   useLayoutEffect(() => {
     navigation.setOptions({
       headerBackTitleVisible: false,
@@ -16,9 +22,44 @@ const Report = ({ navigation, route }) => {
     });
   });
 
+  const handleReport = async (type) => {
+    if (req.report_user_idx == req.reported_user_idx) {
+      toast.show("자신의 의뢰는 신고할 수 없습니다.");
+      navGo.back();
+      return;
+    }
+
+    const res = await API.PUT({
+      url: "/report",
+      data: {
+        report_user_idx: req.report_user_idx,
+        reported_request_idx: req.reported_request_idx,
+        reported_user_idx: req.reported_user_idx,
+        report_type: type,
+      },
+    });
+
+    if (res) toast.show("신고가 완료되었습니다.");
+    else toast.show("이미 신고한 게시글입니다.");
+    navGo.back();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Report</Text>
+      {filter.map((item, index) => {
+        return (
+          <TouchableOpacity
+            key={index}
+            style={styles.item}
+            activeOpacity={0.5}
+            onPress={() => {
+              handleReport(item);
+            }}
+          >
+            <Text style={styles.itemText}>{item}</Text>
+          </TouchableOpacity>
+        );
+      })}
     </View>
   );
 };
@@ -28,7 +69,18 @@ export default Report;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+  },
+  item: {
+    width: "100%",
+    height: 50,
+    borderBottomWidth: 0.3,
+    borderBottomColor: "gray",
+    justifyContent: "center",
+    paddingLeft: 20,
+  },
+  itemText: {
+    fontSize: 16,
+    color: theme["default-text"],
   },
 });
